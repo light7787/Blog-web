ed4b34b28fa73e139609970c18e137718832a168
light7787 <chinufreefire1233@gmail.com>
2024-07-13 22:00:12 +0530

    m

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -27,7 +27,7 @@ const BlogList = () => {
             <button onClick={()=>setMenu('Lifestyle')} className={menu==="Lifestyle"?'bg-black text-white py-1 px-4 rounded-sm':''}>Lifestyle</button>
         
         </div>
-        <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+        <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 ">
             {filteredData.map((item,index)=>{
                 return <BlogItem key={index} id={item._id} image={item.image} title={item.title} description={item.description} category={item.category}/>
 
