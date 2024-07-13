fd9dc0f2a5d2e3c54ebab2a02ea3d77249b4d7d6
light7787 <chinufreefire1233@gmail.com>
2024-07-13 21:48:48 +0530

    header

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -30,17 +30,17 @@ const Header = () => {
   };
 
   return (
-    <div className='py-5 px-5 md:px-12 lg:px-28'>
+    <div className='py-5 px-5 '>
       <div className="flex justify-between items-center">
-        <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
-        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow}/></button>
+        <Image src={assets.logo} width={180} alt='' className='w-[130px] '/>
+        <button className='flex items-center gap-2 font-medium py-1 px-3  border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow}/></button>
       </div>
       <div className='text-center my-8'>
-        <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
-        <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis ducimus tenetur corrupti repellendus, temporibus deserunt nam ab excepturi sit dicta sequi sint aperiam corporis, minima accusamus illo ex esse fugiat.</p>
+        <h1 className='text-3xl font-medium'>Latest Blogs</h1>
+        <p className='mt-10 max-w-[740px] m-auto text-xs '>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis ducimus tenetur corrupti repellendus, temporibus deserunt nam ab excepturi sit dicta sequi sint aperiam corporis, minima accusamus illo ex esse fugiat.</p>
         <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' onSubmit={handleSubmit}>
           <input type="email" placeholder='Enter your email' className='pl-4 outline-none' value={email} onChange={handleFunction}/>
-          <button type='submit' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white'>Subscribe</button>
+          <button type='submit' className='border-l border-black py-4 px-4  active:bg-gray-600 active:text-white'>Subscribe</button>
         </form>
       </div>
     </div>
